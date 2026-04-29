f12bca3a53d7fb78c9ba3becf61105f0761676c0
Jeremy Fernandez <165208679+JeremyF003@users.noreply.github.com>
2026-04-29 11:41:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ function scrollToProjects() {
     behavior: "smooth"
   });
 }
+
+const text = "Jeremy Fernandez";
+let index = 0;
+
+function typeEffect() {
+  if (index < text.length) {
+    document.getElementById("typing-name").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, 80);
+  }
+}
+
+window.onload = typeEffect;
